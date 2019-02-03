d6839e9978b35b6b580f0e372617f9c18d3ccf93
Steve Mann <stevemann2705@gmail.com>
2019-02-04 01:41:31 +0530

    Implementing toString() for Book

--- src/main/java/in/stevemann/spring5webapp/model/Book.java
+++ src/main/java/in/stevemann/spring5webapp/model/Book.java
@@ -87,4 +87,15 @@ public class Book {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
 }
