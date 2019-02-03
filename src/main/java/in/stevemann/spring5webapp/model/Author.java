5e4ff4d8910355cd244324a4e8b8c6c9884e600d
Steve Mann <stevemann2705@gmail.com>
2019-02-04 01:38:51 +0530

    Implementing hashcode() and equals() for Author

--- src/main/java/in/stevemann/spring5webapp/model/Author.java
+++ src/main/java/in/stevemann/spring5webapp/model/Author.java
@@ -2,6 +2,7 @@ package in.stevemann.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -60,5 +61,18 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
